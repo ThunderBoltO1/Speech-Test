9ac6b314a325f5e48b3743e15ff68db3527d7d32
ThunderBoltO1 <pongsaton.pak1998@gmail.com>
2025-03-24 09:39:41 +0700

    update code

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: [
+        './public/**/*.html',
+        './public/**/*.js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['"IBM Plex Sans Thai"', 'sans-serif'], // Add custom font
+            },
+        },
+    },
+    plugins: [],
+};
